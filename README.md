24b36c2d833355bb484f0db568a86ef9c991e490
KazusaNakagawa <kazusanakagawa@gmail.com>
2024-07-20 09:01:47 +0900

    refctor: cdk定義の共通化

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ## Useful commands
 
  * `cdk ls`          list all stacks in the app
- * `cdk synth`       emits the synthesized CloudFormation template
+ * `cdk synth` emits the synthesized CloudFormation template
  * `cdk deploy --context env={{environment}}` deploy this stack to your default AWS account/region
  * `cdk diff`        compare deployed stack with current state
  * `cdk docs`        open CDK documentation
--- lib/cdk_python_stack.py
+++ lib/cdk_python_stack.py
@@ -1,4 +1,5 @@
 import os
+import aws_cdk as cdk
 from aws_cdk import (
     Stack,
     aws_lambda as _lambda,
@@ -9,62 +10,83 @@ from aws_cdk import (
 from constructs import Construct
 from dotenv import load_dotenv
 
+load_dotenv()
+
+# 環境変数からバケット名を取得
+SOURCE_BUCKET = os.environ["SOURCE_BUCKET"]
+TARGET_BUCKET = os.environ["TARGET_BUCKET"]
+
 
 class CdkProjectStack(Stack):
     """CDK Stack class"""
-    load_dotenv()
-
-    # 環境変数からバケット名を取得
-    SOURCE_BUCKET = os.environ["SOURCE_BUCKET"]
-    TARGET_BUCKET = os.environ["TARGET_BUCKET"]
 
     def __init__(self, scope: Construct, construct_id: str, env: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # Lambda関数を定義
-        s3_copy_lambda = _lambda.Function(
-            self,
-            "s3copyHandler",
-            runtime=_lambda.Runtime.PYTHON_3_12,
-            code=_lambda.Code.from_asset("handler"),
-            handler="s3copy.handler",
-            function_name=f"s3copyHandler-{env}",
-            environment={
-                "SOURCE_BUCKET": self.SOURCE_BUCKET,
-                "TARGET_BUCKET": self.TARGET_BUCKET,
-            },
-        )
-
         # 既存のS3バケットを参照
-        source_bucket = s3.Bucket.from_bucket_name(self, "ExistingSourceBucket", self.SOURCE_BUCKET)
-        target_bucket = s3.Bucket.from_bucket_name(self, "ExistingTargetBucket", self.TARGET_BUCKET)
+        source_bucket = s3.Bucket.from_bucket_name(self, "ExistingSourceBucket", SOURCE_BUCKET)
 
-        # Lambda関数に必要なS3バケットへのアクセス権限を付与
-        s3_copy_lambda.add_to_role_policy(
-            iam.PolicyStatement(
-                actions=[
-                    "s3:GetObject",
-                    "s3:ListBucket",
-                    "s3:GetBucketNotificationConfiguration",
-                    "s3:PutBucketNotificationConfiguration",
-                    "s3:PutObject",
-                ],
-                resources=[
-                    source_bucket.bucket_arn,
-                    f"{source_bucket.bucket_arn}/*"
-                ],
-            )
+        # カスタムポリシーを定義
+        custom_policy = iam.Policy(
+            self, "CustomPolicy",
+            statements=[
+                iam.PolicyStatement(
+                    actions=[
+                        "s3:GetObject",
+                        "s3:ListBucket",
+                        "s3:GetBucketNotificationConfiguration",
+                        "s3:PutBucketNotificationConfiguration",
+                        "s3:PutObject",
+                    ],
+                    effect=iam.Effect.ALLOW,
+                    resources=["arn:aws:s3:::*"],
+                ),
+                iam.PolicyStatement(
+                    actions=[
+                        "logs:CreateLogGroup",
+                        "logs:CreateLogStream",
+                        "logs:PutLogEvents",
+                    ],
+                    effect=iam.Effect.ALLOW,
+                    resources=[f"arn:aws:logs:{cdk.Aws.REGION}:{cdk.Aws.ACCOUNT_ID}:log-group:/aws/lambda/*"],
+                ),
+            ],
         )
 
-        # S3バケットにLambda関数へのアクセス権限を付与
-        source_bucket.grant_read_write(s3_copy_lambda)
-        target_bucket.grant_read_write(s3_copy_lambda)
+        # IAMロールを定義し、カスタムポリシーをアタッチ
+        custom_role = iam.Role(self, "CustomRole", assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"))
+        custom_policy.attach_to_role(custom_role)
+
+        # Lambda関数を定義
+        default_lambda = DefaultLambda(
+            self,
+            "defaultHandler",
+            role=custom_role,
+            function_name=f"defaultHandler-{env}",
+        )
 
         # LambdaトリガーにS3バケットを設定
         source_bucket.add_event_notification(
             s3.EventType.OBJECT_CREATED,
-            s3_notifications.LambdaDestination(s3_copy_lambda),
+            s3_notifications.LambdaDestination(default_lambda),
             s3.NotificationKeyFilter(
                 prefix="input/",
-                suffix=".json"),
+                suffix=".json",
+            ),
+        )
+
+
+class DefaultLambda(_lambda.Function):
+    def __init__(self, scope: Construct, id: str, **kwargs):
+        super().__init__(
+            scope,
+            id,
+            runtime=_lambda.Runtime.PYTHON_3_12,
+            code=_lambda.Code.from_asset("handler"),
+            handler="s3copy.handler",
+            environment={
+                "SOURCE_BUCKET": SOURCE_BUCKET,
+                "TARGET_BUCKET": TARGET_BUCKET,
+            },
+            **kwargs  # 他の任意の引数を受け入れる
         )
